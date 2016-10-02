5cd9802e629b261e1521bfee7fc210de3d16fa65
alkin kasap <alkinkasap@users.noreply.github.com>
2016-10-02 20:25:17 +0300

    Delete admin.php

--- site/accounts/admin.php
+++
@@ -1,8 +0,0 @@
-<?php if(!defined('KIRBY')) exit ?>
-
-username: admin
-email: admin@alkinkasap.net
-password: >
-  $2a$10$44GiyVSphs7Lvnqy4wbhC.2G.efZ5IKK9ldWAewQcGxZk.VR5D1RC
-language: tr
-role: admin
